7f128a12932fed62f9e1b512153f14adfb0efb5c
유승연 <seong1542@naver.com>
2023-06-04 20:21:53 +0900

    feat: add init files

---
+++ src/test/java/com/nhnacademy/taskApi/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.taskApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
