c7cae36e12c3de44fdf1c2584d61184c2850ce98
정찬영 <chung4171@naver.com>
2023-06-11 14:03:59 +0900

    프로젝트와 마일스톤 서비스 개발

---
+++ src/main/java/com/nhnacademy/taskApi/advice/ApiControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.taskApi.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+}
---
+++ src/main/java/com/nhnacademy/taskApi/exception/milestone/MilestoneNotFoundIdException.java
@@ -0,0 +1,2 @@
+package com.nhnacademy.taskApi.exception.milestone;public class MilestoneNotFoundIdException {
+}
